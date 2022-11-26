75fc367838d5afe05aee3da42db079e9d7573518
clerici03 <115361946+clerici03@users.noreply.github.com>
2022-11-26 14:03:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+   int n=0;
+    int a=1, b=1, c=0;
+    cin>>n;
+    cout<< a <<endl;
+    cout<< b <<endl;
+    int i=0;
+    for (i =2;i < n;i++) {
+        c = a + b;
+        a = b;
+        b = c;
+        cout << c << endl;
    return 0;
 }
